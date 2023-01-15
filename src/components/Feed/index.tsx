682c16e737418b55f89d878be6a27182ba600e41
chelseadgls <chelseamdouglas@gmail.com>
2023-01-15 13:26:15 -0500

    working

--- src/components/Feed/index.tsx
+++ src/components/Feed/index.tsx
@@ -21,26 +21,7 @@ function Feed() {
 
   return (
     <div className="grid gap-8 py-8">
-        {
-          feed.map((story) => {
-            return (
-              <div className="flex border-2 border-gray-200 border-solid px-8 py-8">
-                <img src={story['thumbnail']['desktop']} alt="" height="100" width="200" className="object-contain" />
-              <div className="flex flex-col justify-between">
-              <div className="px-4">  
-                <h1 className="font-bold text-xl">{story['title']}</h1>
-              </div>
-                <div>
-                  <div className="flex flex-row px-4">
-                      <img src={story['author']['avatar']} alt="" height="50" width="40" className="object-contain rounded-full" />
-                      <p className="px-4 py-2">{story['author']['name']}</p>
-                  </div>
-              </div>
-              </div>
-            </div>
-            )
-          })
-        }
+      {feed.map(story => <Story story={story} />)}
       </div>
   )
 }
--- src/components/Story/index.tsx
+++ src/components/Story/index.tsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from "react";
 
-function Story() {
+function Story({story}) {
   return (
     <div>
-
+      <div className="flex border-2 border-gray-200 border-solid px-8 py-8">
+        <img
+          src={story["thumbnail"]["desktop"]}
+          alt=""
+          height="100"
+          width="200"
+          className="object-contain"
+        />
+        <div className="flex flex-col justify-between">
+          <div className="px-4">
+            <h1 className="font-bold text-xl">{story["title"]}</h1>
+          </div>
+          <div>
+            <div className="flex flex-row px-4">
+              <img
+                src={story["author"]["avatar"]}
+                alt=""
+                height="50"
+                width="40"
+                className="object-contain rounded-full"
+              />
+              <p className="px-4 py-2">{story["author"]["name"]}</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Story
+export default Story;
