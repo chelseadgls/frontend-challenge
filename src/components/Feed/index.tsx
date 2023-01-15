4659cb8668bf09925b791143b1f32ccc2b32eb7d
chelseadgls <chelseamdouglas@gmail.com>
2023-01-14 20:07:50 -0500

    feed setup

--- src/components/Feed/index.tsx
+++ src/components/Feed/index.tsx
@@ -20,19 +20,21 @@ function Feed() {
   })
 
   return (
-    <div className="border-8 border-black border-solid">
+    <div className="grid gap-8 py-8">
         {
           feed.map((story) => {
             return (
-              <div className="flex border-8 border-red-500 border-solid px-8 py-8">
-                <img src={story['thumbnail']['desktop']}/>
-              <div>
-              <div>  
-                <h1>{story['title']}</h1>
+              <div className="flex border-2 border-gray-200 border-solid px-8 py-8">
+                <img src={story['thumbnail']['desktop']} alt="" height="100" width="200" className="object-contain" />
+              <div className="flex flex-col justify-between">
+              <div className="px-4">  
+                <h1 className="font-bold text-xl">{story['title']}</h1>
               </div>
-              <div>
-                  <p>{story['author']['name']}</p>
-                  <img src={story['author']['avatar']} />
+                <div>
+                  <div className="flex flex-row px-4">
+                      <img src={story['author']['avatar']} alt="" height="50" width="40" className="object-contain rounded-full" />
+                      <p className="px-4 py-2">{story['author']['name']}</p>
+                  </div>
               </div>
               </div>
             </div>
